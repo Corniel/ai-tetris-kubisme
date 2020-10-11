42e2470cf44f58f488f5b0e8fff1d4f3b325e286
Corniel Nobel <mail@corniel.nl>
2020-10-11 19:13:04 +0200

    Introduction of NextShape interface.

---
+++ src/Tetris/Randomization/SimpleGenerator.cs
@@ -0,0 +1,21 @@
+﻿using SmartAss;
+using Troschuetz.Random;
+using Troschuetz.Random.Generators;
+
+namespace Tetris.Randomization
+{
+    public class SimpleGenerator : NextShape
+    {
+        public SimpleGenerator() : this(null) => Do.Nothing();
+
+        public SimpleGenerator(int seed)
+            : this(new MT19937Generator(seed)) => Do.Nothing();
+
+        public SimpleGenerator(IGenerator generator)
+            => Rnd = generator ?? new MT19937Generator();
+
+        protected IGenerator Rnd { get; }
+
+        public virtual Shape Next() => (Shape)Rnd.Next(8);
+    }
+}
--- test/Testris.Specs/RandomGenerator_specs.cs
+++ test/Testris.Specs/RandomGenerator_specs.cs
@@ -1,9 +1,8 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Tetris;
+using Tetris.Randomization;
 
 namespace RandomGenerator_specs
 {
